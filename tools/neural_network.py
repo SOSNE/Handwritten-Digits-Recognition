a3eec9a7fbafd976acc1867bf621a1bfbffe114a
SOSNE <tomjansan@gmail.com>
2024-07-01 21:21:09 +0200

    Added bias calculation in back propagation.

--- tools/neural_network.py
+++ tools/neural_network.py
@@ -47,7 +47,7 @@ def forward_propagation():
 
 
 def back_propagation():
-    global weight_last, weight_first
+    global weight_last, weight_first, bias_last, bias_first
 
     hidden_layer_values, output_values, sigmoid_hidden_layer_values, sigmoid_output_values, output_values_true = forward_propagation()
 
@@ -56,15 +56,20 @@ def back_propagation():
     gradient_output_values = (gradient_sigmoid_output_values_loss * sigmoid_derivative(output_values))
 
     gradient_weight_output = np.dot(gradient_output_values.T, sigmoid_hidden_layer_values).T
+    gradient_bias_output = np.sum(gradient_output_values, 0)
 
     gradient_hidden_loss = np.dot(gradient_output_values, weight_last.T)
 
     gradient_hidden_values = (gradient_hidden_loss * sigmoid_derivative(hidden_layer_values))
 
     gradient_weight_first = np.dot(input_data.T, gradient_hidden_values)
+    gradient_bias_first = np.sum(gradient_hidden_values, 0)
 
-    weight_last = weight_last - gradient_weight_output * 0.0001
-    weight_first = weight_first - gradient_weight_first * 0.0001
+    weight_last = weight_last - gradient_weight_output * 0.001
+    weight_first = weight_first - gradient_weight_first * 0.001
+
+    bias_last = bias_last - gradient_bias_output * 0.0001
+    bias_first = bias_first - gradient_bias_first * 0.0001
 
 
 for i in range(1000):
