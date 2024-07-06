26ba5cd33255ffac709c10a3d9e37077fa17cfb5
SOSNE <tomjansan@gmail.com>
2024-07-06 15:09:22 +0200

    This name will be fixed in next training.

--- tools/utils.py
+++ tools/utils.py
@@ -1,14 +1,14 @@
 import json, os
 
 
-def save_model(data, path="../store/weights.json"):
+def save_model(data, path="../store/model.json"):
     data = json.dumps(data)
     os.makedirs(os.path.join("../store"), exist_ok=True)
     with open(path, 'w') as f:
         f.write(data)
 
 
-def load_model(path="../store/model.json"):
+def load_model(path="../store/weights.json"):
     with open(path, 'r') as f:
         data = f.read()
         return json.loads(data)
