691aa5d9108a122803a04f10c65eb5e869deec76
SOSNE <85309481+SOSNE@users.noreply.github.com>
2024-07-08 00:43:06 +0200

    Add drawing system (#2)
    * Implemented drawing logic using pygame.
    
    * Added processing image to convert it to array.
    
    * Implemented start_drawing func in main. Small bug fix in draw.py

--- main.py
+++ main.py
@@ -2,26 +2,33 @@ import matplotlib.pyplot as plt
 from tools.file_reader import read_file_image, read_file_label
 from tools.model import predict_output
 from tools.utils import load_model
+from tools.draw import start_drawing
 import random
 
 images = read_file_image("data/t10k-images-idx3-ubyte")
 labels = read_file_label("data/t10k-labels-idx1-ubyte")
 
 model = load_model("store/weights.json")
-bad_images = []
+# bad_images = []
 
-for index in range(len(images)-1):
-    # image_index = random.randint(0, len(images) - 1)
+img = start_drawing()
 
-    predicted_value, _ = predict_output(images[index], model[0], model[1], model[2], labels[index])
+predicted_value, _ = predict_output(img, model[0], model[1], [10, 10], labels[0])
 
-    if predicted_value != labels[index]:
-        print("Label:", labels[index])
-        print("Predicted label:", predicted_value)
-        bad_images.append({f"correct: {labels[index]} not: ": predicted_value})
+print("Predicted label:", predicted_value)
 
-print(len(images))
-print(len(bad_images))
+# for index in range(len(images)-1):
+#     # image_index = random.randint(0, len(images) - 1)
+#
+#     predicted_value, _ = predict_output(images[index], model[0], model[1], model[2], labels[index])
+#
+#     if predicted_value != labels[index]:
+#         print("Label:", labels[index])
+#         print("Predicted label:", predicted_value)
+#         bad_images.append({f"correct: {labels[index]} not: ": predicted_value})
+#
+# print(len(images))
+# print(len(bad_images))
 # plt.imshow(images[image_index], cmap='gray')
 # plt.show()
 
---
+++ tools/draw.py
@@ -0,0 +1,57 @@
+import pygame
+import numpy as np
+import cv2
+
+color = (255, 255, 255)
+width = 7
+
+
+pygame.init()
+screen = pygame.display.set_mode((280, 280))
+clock = pygame.time.Clock()
+
+
+def draw_line(start, end):
+    dx = end[0] - start[0]
+    dy = end[1] - start[1]
+    distance = max(abs(dx), abs(dy))
+    for i in range(distance):
+        x = int(start[0] + float(i) / distance * dx)
+        y = int(start[1] + float(i) / distance * dy)
+        pygame.draw.circle(screen, color, (x, y), width)
+
+
+def process_image(surface):
+    pixel_array = pygame.surfarray.pixels2d(surface)
+    pixel_array = np.array(pixel_array, dtype=np.uint8)
+    resized_image = cv2.resize(pixel_array, (28, 28))
+    return resized_image
+
+
+screen.fill("black")
+
+
+def start_drawing():
+    image = None
+    drawing = False
+    last_pos = None
+    while True:
+        mouse_pos = pygame.mouse.get_pos()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                return image
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                pygame.draw.circle(screen, color, mouse_pos, width)
+                last_pos = mouse_pos
+                drawing = True
+            elif event.type == pygame.MOUSEBUTTONUP:
+                image = process_image(screen)
+                drawing = False
+            elif event.type == pygame.MOUSEMOTION and drawing:
+                draw_line(last_pos, mouse_pos)
+                last_pos = mouse_pos
+
+        pygame.display.flip()
+
+        clock.tick(60)
