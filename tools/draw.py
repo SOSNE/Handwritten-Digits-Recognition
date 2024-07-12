e1f6d4ec12fc521ce766be1705cf56282321ea55
SOSNE <tomjansan@gmail.com>
2024-07-12 14:48:15 +0200

    Added centering of drawn image.

--- tools/draw.py
+++ tools/draw.py
@@ -1,10 +1,10 @@
 import pygame
 import numpy as np
 import cv2
+from scipy.ndimage import shift
 
 color = (255, 255, 255)
 width = 13
-
 
 pygame.init()
 screen = pygame.display.set_mode((280, 280))
@@ -21,12 +21,24 @@ def draw_line(start, end):
         pygame.draw.circle(screen, color, (x, y), width)
 
 
+def center_image(image):
+    col_sum = np.nonzero(np.sum(image, axis=0))
+    row_sum = np.nonzero(np.sum(image, axis=1))
+    y1, y2 = row_sum[0][0], row_sum[0][-1]
+    x1, x2 = col_sum[0][0], col_sum[0][-1]
+    row_mov = ((27 - y2) - y1) / 2
+    col_mov = ((27 - x2) - x1) / 2
+    image = shift(image, shift=[row_mov, col_mov])
+    return image
+
+
 def process_image(surface):
     pixel_array = pygame.surfarray.pixels2d(surface)
     pixel_array = np.array(pixel_array, dtype=np.uint8)
     pixel_array = np.transpose(pixel_array)
     resized_image = cv2.resize(pixel_array, (28, 28))
-    return resized_image
+    centered_image = center_image(resized_image)
+    return centered_image
 
 
 screen.fill("black")
